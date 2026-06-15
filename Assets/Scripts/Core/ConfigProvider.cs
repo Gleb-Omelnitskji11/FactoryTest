0a1945bbb2289df42e8b99bcf7154ee579d7b426
Glib <omelniskij@gmail.com>
2026-06-15 18:29:37 +0300

    Move config to Resources

--- Assets/Scripts/Core/ConfigProvider.cs
+++ Assets/Scripts/Core/ConfigProvider.cs
@@ -1,16 +1,24 @@
 using ConfigData;
 using UnityEngine;
-using UnityEngine.Serialization;
 
 namespace Core
 {
     public class ConfigProvider : MonoBehaviour
     {
-        [FormerlySerializedAs("_unitConfig")] [SerializeField] private GameConfig _gameConfig;
+        private GameConfig _gameConfig;
+        private const string GameConfigFileName = "GameConfig";
 
         private void Start()
         {
             DontDestroyOnLoad(this);
+        }
+
+        public async Awaitable InitializeConfigs()
+        {
+            ResourceRequest operation  = Resources.LoadAsync<GameConfig>(GameConfigFileName);
+            await operation;
+            if (operation.isDone)
+                _gameConfig = operation.asset as GameConfig;
         }
 
         public GameConfig GameConfig => _gameConfig;
--- Assets/Scripts/Preloader/PreloadController.cs
+++ Assets/Scripts/Preloader/PreloadController.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using Core;
+using Cysharp.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using Zenject;
@@ -9,33 +11,45 @@ namespace Preloader
     public class PreloadController : MonoBehaviour
     {
         private PlayerProgressSaver _playerProgressSaver;
+        private ConfigProvider _configProvider;
 
         [Inject]
-        public void Construct(PlayerProgressSaver playerProgressSaver)
+        public void Construct(PlayerProgressSaver playerProgressSaver, ConfigProvider configProvider)
         {
+            _configProvider = configProvider;
             _playerProgressSaver = playerProgressSaver;
         }
 
         private void Start()
         {
+            StartTheGame();
+        }
+
+        private async void StartTheGame()
+        {
+            await LoadConfig();
             InitPlayerPrefs();
             GoToGame();
         }
 
+        private async UniTask LoadConfig()
+        {
+            await _configProvider.InitializeConfigs();
+        }
+
+        private void InitPlayerPrefs()
+        {
+            _playerProgressSaver.Initialize();
+        }
+
         private void GoToGame()
         {
-            InitPlayerPrefs();
             StartCoroutine(GoToGameCoroutine());
         }
 
         private IEnumerator GoToGameCoroutine()
         {
             yield return SceneManager.LoadSceneAsync(Constants.GameScene);
-        }
-
-        private void InitPlayerPrefs()
-        {
-            _playerProgressSaver.Initialize();
         }
     }
 }
